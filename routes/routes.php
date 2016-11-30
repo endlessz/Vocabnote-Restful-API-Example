72134bffebcb65411fc40d2127107833d618152e
Wasut <wasut_pao@hotmail.com>
2016-11-30 20:09:22 +0700

    rename web.php to routes.php, add routes getVocab

--- routes/routes.php
+++ routes/routes.php
@@ -14,3 +14,11 @@
 $app->get('/', function () use ($app) {
     return $app->version();
 });
+
+
+$app->group(['prefix' => 'api/v1'], function($app)
+{
+  
+    $app->get('vocabs/{id}','VocabController@getVocab');
+      
+});
