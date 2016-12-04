47b13c5bbecc8dd7c4611f954e1bbf7e67b39f58
Wasut <wasut_pao@hotmail.com>
2016-12-04 15:25:27 +0700

    add seeder create User tester1

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +13,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call('UsersTableSeeder');
+        User::create([
+            'email' => 'tester1@gmail.com', 
+            'username' => 'tester1',
+            'password' => Hash::make('password')
+        ]);
     }
 }
