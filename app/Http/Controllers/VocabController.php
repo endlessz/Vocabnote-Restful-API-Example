45b59d2f15ff084dd644307df299ea5a5b4aeb86
Wasut <wasut_pao@hotmail.com>
2016-12-04 15:28:33 +0700

    refactor all method to can CRUD only their own vocab

--- app/Http/Controllers/VocabController.php
+++ app/Http/Controllers/VocabController.php
@@ -5,32 +5,34 @@ namespace App\Http\Controllers;
 use Validator;
 use Illuminate\Http\Request;
 use App\Models\Vocab;
-
 use App\Helpers\Responses;
+use Tymon\JWTAuth\JWTAuth;
 
 class VocabController extends Controller
 {
     protected $vocab;
+    protected $jwt;
 
-    public function __construct(Vocab $vocab)
+    public function __construct(Vocab $vocab, JWTAuth $jwt)
     {
         $this->vocab = $vocab;
+        $this->jwt = $jwt;
     }
 
     public function getVocabs(Request $request)
     {
-        $vocabs = $this->vocab->getVocabs($request->input('size'));
+        $user = $this->jwt->parseToken()->authenticate();
+        $vocabs = $user->vocabs();
+        $size = $request->input('size');
 
-        if(!$vocabs){
-            return Responses::notFound('This vocab is not found.');
-        }
-
-        return Responses::json($vocabs);
+        return Responses::json($vocabs->paginate(is_numeric($size) ? intval($size) : 10));
     }
 
     public function getVocab($id)
     {
-        $vocab = $this->vocab->getVocab($id);
+        $user = $this->jwt->parseToken()->authenticate();
+
+        $vocab = $user->vocabs()->find($id);
 
         if(!$vocab){
             return Responses::notFound('This vocab is not found.');
@@ -50,14 +52,22 @@ class VocabController extends Controller
             return Responses::badRequest($validator->errors());
         }
 
-        $vocab = $this->vocab->createVocab($request);
+        $user = $this->jwt->parseToken()->authenticate();
+        $vocab = $user->vocabs()->create([
+                            'word' => $request->input('word'), 
+                            'meaning' => $request->input('meaning'),
+                            'example' => $request->input('example'),
+                        ]);
 
         return Responses::created($vocab);
     }
 
     public function updateVocab(Request $request)
     {
-        $vocab = $this->vocab->getVocab($request->input('id'));
+        $user = $this->jwt->parseToken()->authenticate();
+
+        $vocab = $user->vocabs()->find($request->input('id'));
+        
         if(!$vocab){
             return Responses::notFound('This vocab is not found.');
         }
@@ -79,7 +89,10 @@ class VocabController extends Controller
 
     public function deleteVocab($id)
     {
-        $vocab = $this->vocab->getVocab($id); 
+        $user = $this->jwt->parseToken()->authenticate();
+
+        $vocab = $user->vocabs()->find($id);
+
         if(!$vocab){
             return Responses::notFound('This vocab is not found.');
         }
