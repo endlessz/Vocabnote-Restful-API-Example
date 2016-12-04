495e4e849ac64a8cbf9d00b18aeb26921a90d33b
Wasut <wasut_pao@hotmail.com>
2016-12-04 15:17:36 +0700

    refactor response to use unauthorized of Responses helper class

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Contracts\Auth\Factory as Auth;
+use App\Helpers\Responses;
 
 class Authenticate
 {
@@ -36,7 +37,7 @@ class Authenticate
     public function handle($request, Closure $next, $guard = null)
     {
         if ($this->auth->guard($guard)->guest()) {
-            return response('Unauthorized.', 401);
+            return Responses::unauthorized('Unauthorized. Please login again.');
         }
 
         return $next($request);
