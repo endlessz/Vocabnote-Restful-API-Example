7eb9b92fd46f523d79c328df4981049b0e4234b2
Wasut <wasut_pao@hotmail.com>
2016-12-04 15:16:52 +0700

    add routes login using AuthController@postLogin, refactor routes of vocab to pass middleware auth:api

--- routes/routes.php
+++ routes/routes.php
@@ -4,18 +4,26 @@ $app->get('/', function () use ($app) {
     return $app->version();
 });
 
-
 $app->group(['prefix' => 'api/v1'], function($app)
 {  
 	$app->post('register','AuthController@createUser');
 
-	$app->get('vocabs','VocabController@getVocabs');
+	$app->post('login', 'AuthController@postLogin');
 
-    $app->get('vocabs/{id}','VocabController@getVocab');
-      
-    $app->post('vocabs','VocabController@createVocab');
-      
-    $app->put('vocabs','VocabController@updateVocab');
-      
-    $app->delete('vocabs/{id}','VocabController@deleteVocab');
+	$app->group(['middleware' => 'auth:api'], function($app)
+	{
+		$app->get('user', 'AuthController@getAuthenticatedUser');
+
+		$app->get('vocabs/{id}','VocabController@getVocab');
+
+		$app->get('vocabs','VocabController@getVocabs');
+
+	    $app->get('vocabs/{id}','VocabController@getVocab');
+	      
+	    $app->post('vocabs','VocabController@createVocab');
+	      
+	    $app->put('vocabs','VocabController@updateVocab');
+	      
+	    $app->delete('vocabs/{id}','VocabController@deleteVocab');
+	});
 });
