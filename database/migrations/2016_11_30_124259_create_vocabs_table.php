90fc4114e90e53954c39fdab7007ca1d82e0452c
Wasut <wasut_pao@hotmail.com>
2016-12-04 15:18:30 +0700

    add column user_id

--- database/migrations/2016_11_30_124259_create_vocabs_table.php
+++ database/migrations/2016_11_30_124259_create_vocabs_table.php
@@ -18,6 +18,7 @@ class CreateVocabsTable extends Migration
             $table->string('word');
             $table->string('meaning');
             $table->text('example')->nullable();
+            $table->integer('user_id');
             $table->timestamps();
         });
     }
